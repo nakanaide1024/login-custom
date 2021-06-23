b3b74c6b4d7a03bd5ec7f1e9b09ab6dc52c2918c
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-23 22:22:56 +0900

    showLoginメソッドの作成

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
+    /**
+     * @return View
+     */
+    public function showLogin(){
+
+        return view('login.login_form');
+        
+    }
 }
