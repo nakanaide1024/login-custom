ba33524af370f577fd3c304071fbe2e3285104e1
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-23 22:15:49 +0900

    ログインページとログイン処理のルート記述

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Auth\AuthController;
+use Laravel\Ui\AuthCommand;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
+
+Route::post('Login', [AuthController::class, 'login'])->name('login');
