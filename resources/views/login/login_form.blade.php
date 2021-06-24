06dc6777d3a8bfc1f795b374ab3d59e747d20857
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-24 23:36:47 +0900

    html雛形作成とcss,jsの読み込みのための記述

---
+++ resources/views/login/login_form.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログインフォーム</title>
+  <!-- Script -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+  
+</body>
+</html>
