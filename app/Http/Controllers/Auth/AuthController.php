8e6ba6faa13dc30779ce48eb99038bfd9df4d898
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-06-26 20:46:32 +0900

    AuthコントローラーでのLoginFormRequestファイルの引用

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,6 +14,14 @@ class AuthController extends Controller
     public function showLogin(){
 
         return view('login.login_form');
-        
+
+    }
+
+    /**
+     *  @param App\Http\Requests\LoginFormRequest $request
+     */
+    public function login(LoginFormRequest $request){
+
+        dd($request->all());
     }
 }
