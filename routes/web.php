5ec63bf9c829222fef8dd12f4a8046c9c41a7209
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-07-03 17:41:30 +0900

    独自のルートミドルウェアの作成

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,18 @@ use Laravel\Ui\AuthCommand;
 |
 */
 
-Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
 
-Route::post('Login', [AuthController::class, 'login'])->name('login');
+Route::group(['middleware' => ['guest']], function(){
+  //ログインホーム表示
+  Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
+  //ログイン処理
+  Route::post('Login', [AuthController::class, 'login'])->name('login');
+});
 
-Route::get('home',function(){
-  return view('home');
-})->name('home');
+
+Route::group(['middleware' => ['auth']], function(){
+    //ホーム画面
+    Route::get('home', function () {
+        return view('home');
+    })->name('home');
+});
